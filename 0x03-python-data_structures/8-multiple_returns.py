1008c35acd2bb1fb630edb9c895a86134e00532a
AhmedSamy98 <ahmed28samy@gmail.com>
2023-09-11 18:14:26 -0700

    8. More returns!

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        sentence[i] = None
+    i = 0
+    length = len(sentence)
+    tuple_a = (length, sentence[i])
+    return tuple_a
