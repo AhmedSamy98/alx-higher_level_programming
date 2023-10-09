c1c75aaa556d65be54f0e999167de08ea45d58c3
AhmedSamy98 <ahmed1samy1998@gmail.com>
2023-10-09 23:44:08 +0000

    0. Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def lookup(obj):
+    """Returns the list of the attributes and methods of obj
+    Args: obj (_type_): an object to be used
+    Returns: dir(obj)
+    """
+    return dir(obj)
