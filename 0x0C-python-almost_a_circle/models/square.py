a6edb8f0e4c9fb82d6cce9303863509c5e699ee2
AhmedSamy98 <ahmed1samy1998@gmail.com>
2023-10-17 04:03:05 +0300

    FIX

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,73 +1,78 @@
 #!/usr/bin/python3
-# -*- encoding: utf-8 -*-
 """
-    This is a class module
-    Author: Peter Ekwere
+    Module: Square class inherits Rectangle
+"""
 
-"""
-import sys
 from models.rectangle import Rectangle
-sys.path.append("..")
-
-if __name__ == "__main__":
-    """ Do Not run Dirctly """
 
 
 class Square(Rectangle):
-    """ This is a child class that would inherit from The base class """
+    """
+        Square class inheirts rectangle
+    """
+
     def __init__(self, size, x=0, y=0, id=None):
-        """ This is the __init__ function """
+        """
+            Initializes the instance of Square class
+
+            Args:
+                size: size of the square
+                x: x coordinate of the square
+                y: y coordinate of the square
+                id: id of the square
+        """
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """ Getter for Size """
+        """
+            Getter for size
+        """
         return self.width
 
     @size.setter
     def size(self, value):
-        """ This is Size setter """
+        """
+            Setter for size
+        """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
         self.width = value
         self.height = value
 
-    def __str__(self):
-        """ This function overrides the __str__ call """
-        return f"[Square] ({self.id})" \
-               f" {self.x}/{self.y} - {self.height}"
-
     def update(self, *args, **kwargs):
-        """ This function Updates The class/instance attr """
-        if len(args) > 0:
-            if args[0] is not None:
-                self.id = args[0]
-        if len(args) > 1:
-            if args[1] is not None:
-                self.width = args[1]
-                self.height = args[1]
-        if len(args) > 2:
-            if args[2] is not None:
-                self.x = args[2]
-        if len(args) > 3:
-            if args[3] is not None:
-                self.y = args[3]
-        if len(kwargs) > 0:
+        """
+            Assign an argument to each attribute
+        """
+        if args:
+            for i, arg in enumerate(args):
+                if i == 0:
+                    self.id = arg
+                if i == 1:
+                    self.size = arg
+                if i == 2:
+                    self.x = arg
+                if i == 3:
+                    self.y = arg
+        else:
             for key, value in kwargs.items():
-                if key == 'id':
+                if key == "id":
                     self.id = value
-                elif key == 'size':
-                    self.width = value
-                    self.height = value
-                elif key == 'x':
+                if key == "size":
+                    self.size = value
+                if key == "x":
                     self.x = value
-                elif key == 'y':
+                if key == "y":
                     self.y = value
 
     def to_dictionary(self):
-        """ This is a dictionary function """
-        diction = {
-                'id': self.id,
-                'size': self.width,
-                'x': self.x,
-                'y': self.y
-                }
-        return diction
+        """
+            Returns dict representation of Square Class
+        """
+        return {'id': getattr(self, "id"),
+                'size': getattr(self, "width"),
+                'x': getattr(self, "x"),
+                'y': getattr(self, "y")}
