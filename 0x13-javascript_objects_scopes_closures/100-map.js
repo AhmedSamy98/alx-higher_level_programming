17b651121a2bb721cd3bfb9bab8c394714076ebc
AhmedSamy98 <ahmed28samy@gmail.com>
2023-12-13 04:51:40 +0200

    0x13. JavaScript

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-
-const origList = require('./100-data').list;
-
-const newList = origList.map((elem, idx) => elem * idx);
-console.log(origList);
-console.log(newList);
+const list = require('./100-data').list;
+console.log(list);
+const Newlist = list.map((x, i) => x * i);
+console.log(Newlist);
--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-
 const dict = require('./101-data').dict;
 const newDict = {};
-
-Object.keys(dict).map(function (key) {
-  if (!Array.isArray(newDict[dict[key]])) {
-    newDict[dict[key]] = [];
+for (const key in dict) {
+  if (dict[key] in newDict) {
+    newDict[dict[key]].push(key);
+  } else {
+    newDict[dict[key]] = [key];
   }
-  return newDict[dict[key]].push(key);
-});
-
+}
 console.log(newDict);
