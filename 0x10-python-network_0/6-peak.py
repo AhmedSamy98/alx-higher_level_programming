292fc4902d6fcb1bc13ee0904b2f665d62cb34cc
AhmedSamy98 <ahmed1samy1998@gmail.com>
2024-01-05 14:10:28 -0800

    6. Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,15 +1,35 @@
 #!/usr/bin/python3
-"""a function that finds a peak in a list of unsorted integers"""
+"""
+    This is a peak module
+    Author: Peter Ekwere
+
+"""
+
+
+def isPeak(a_list, size, number, index_i, index_j):
+    """ This function returns true if peak else false
+    """
+    if index_i is not None and a_list[index_i] > number:
+        return False
+
+    if index_j is not None and a_list[index_i] > number:
+        return False
+
+    return True
 
 
 def find_peak(list_of_integers):
+    """ This function returns the peak in a list
     """
-    a function that finds a peak in a list of unsorted integers
-    """
-    if list_of_integers == []:
-        return None
-    for i in range(1, (len(list_of_integers) - 1)):
-        if list_of_integers[i - 1] < list_of_integers[i]\
-        and list_of_integers[i + 1] < list_of_integers[i]:
-            return list_of_integers[i]
-    return (max(list_of_integers[0], list_of_integers[-1]))
+    if not list_of_integers:
+        return
+
+    size = len(list_of_integers)
+    index_j = size - 1
+    new_list = []
+    for index_i in range(1, len(list_of_integers)):
+        number = list_of_integers[index_i]
+        if isPeak(list_of_integers, size, number, index_i, index_j):
+            new_list.append(number)
+    peak = max(new_list)
+    return peak
