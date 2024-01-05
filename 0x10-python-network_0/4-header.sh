e4d143104430a305f6cdc7dafb2f7c458aa22a6b
AhmedSamy98 <ahmed1samy1998@gmail.com>
2024-01-05 13:09:31 -0800

    4. cURL headers

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# a Bash script that sends a custom header variable
-curl -s -L -X GET "$1" -H "X-HolbertonSchool-User-Id: 98"
+# Send a GET request to a given URL with a header variable.
+curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
