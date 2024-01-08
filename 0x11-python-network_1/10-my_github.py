8890958913b0bf5e75001642bea13372b64d0c1c
AhmedSamy98 <ahmed1samy1998@gmail.com>
2024-01-08 02:15:34 +0200

    FIX

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""
-module to print ones github id
-One will need GH PAT for this
+"""Uses the GitHub API to display a GitHub ID based on given credentials.
+Usage: ./10-my_github.py <GitHub username> <GitHub password>
+  - Uses Basic Authentication to access the ID.
 """
 import sys
 import requests
@@ -12,5 +12,3 @@ if __name__ == "__main__":
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
     r = requests.get("https://api.github.com/user", auth=auth)
     print(r.json().get("id"))
-
-
